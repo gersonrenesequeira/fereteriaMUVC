0774235d286dad68092df1f6dd5fb5e8f645b613
gersonrenesequeira <sequeiramedinag@gmail.com>
2025-03-25 10:48:41 -0600

    Inicial proyecto estructura.

---
+++ src/Modelo/DetalleCompra.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class DetalleCompra {
+   private int idDetalleCompra;
+private Compra compra; // Relación con Compra
+private Producto producto; // Relación con Producto
+private int cantidad;
+private float precioUnitario; 
+
+    public int getIdDetalleCompra() {
+        return idDetalleCompra;
+    }
+
+    public void setIdDetalleCompra(int idDetalleCompra) {
+        this.idDetalleCompra = idDetalleCompra;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(float precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public DetalleCompra(int idDetalleCompra, Compra compra, Producto producto, int cantidad, float precioUnitario) {
+        this.idDetalleCompra = idDetalleCompra;
+        this.compra = compra;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public DetalleCompra() {
+    }
+}
